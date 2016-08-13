7509ba531ac071d173a5adc377432af7d93b88e4
Rexet Blell <rexetblell@gmail.com>
2016-08-13 16:47:34 -0700

    Finish implementing GroupPoker

--- GroupPoker/GroupPoker.sol
+++ GroupPoker/GroupPoker.sol
@@ -48,7 +48,7 @@ contract GroupPoker {
         finished
     }
 
-    uint constant MAX_NUM = 1000000;
+    // Account that holds chips.
     TokenInterface token_interface;
 
     Game[] public games;
@@ -58,43 +58,81 @@ contract GroupPoker {
         uint ante_size;
         mapping(address => bytes32) hidden_hands;
         mapping(address => bytes32) hidden_bids;
+        mapping(address => bytes32) hidden_calls;
         mapping(address => uint) max_bid_amounts;
-        uint bet_size;
+        mapping(address => bool) eligible_to_win;
+        bytes32 hand_shift;
         address bettor;
+        uint bet_size;
+        uint pot_size;
+        uint best_hand;
+        address winner;
+        uint time_of_last_stage_change;
+        uint time_per_stage;
+    }
+
+    function next_stage(Game g) internal {
+        if (now > g.time_of_last_stage_change + g.time_per_stage) {
+            g.stage = Stages(uint(g.stage) + 1);
+            g.time_of_last_stage_change = now;
+        }
+    }
+
+    
+    function calculate_hand(uint hand, bytes32 hand_shift) constant returns (uint) {
+        return uint(sha3(hand, hand_shift));
+    }
+
+    function get_current_hand_value(uint game_num, uint hand) constant returns(uint) {
+        Game g = games[game_num];
+        return calculate_hand(hand, g.hand_shift);
     }
 
     function GroupPoker(TokenInterface token_interface_) {
         token_interface = token_interface_;
     }
 
+    function new_game(uint ante_size_, uint time_per_stage_) {
+        games.length++;
+        Game g = games[games.length - 1];
+        g.ante_size = ante_size_;
+        g.time_of_last_stage_change = now;
+        g.time_per_stage = time_per_stage_;
+    }
+
     function submit_hidden_hand(uint game_num, bytes32 hidden_hand) {
         Game g = games[game_num];
+        next_stage(g);
         if (g.stage != Stages.submit_hidden_hand) {
             throw;
         }
-        // transfer ante from player's account
-        result = token_interface.transferFrom(msg.sender, address(this), g.ante_size);
+        g.hidden_hands[msg.sender] = hidden_hand;
+        g.hand_shift = sha3(g.hand_shift, block.blockhash(block.number));
+        g.pot_size += g.ante_size;
+        bool result = token_interface.transferFrom(msg.sender, address(this), g.ante_size);
         if (!result) {
             throw;
         }
-        g.hidden_hands[msg.sender] = hidden_hand;
     }
 
     function submit_hidden_bid(uint game_num, uint max_bid_amount, bytes32 hidden_bid) {
         Game g = games[game_num];
+        next_stage(g);
         if (g.stage != Stages.submit_hidden_bid) {
-            throw;
-        }
-        result = token_interface.transferFrom(msg.sender, address(this), max_bid_amount);
-        if (!result) {
             throw;
         }
         g.hidden_bids[msg.sender] = hidden_bid;
         g.max_bid_amounts[msg.sender] = max_bid_amount;
+        g.pot_size += max_bid_amount;
+        bool result = token_interface.transferFrom(msg.sender, address(this), max_bid_amount);
+        if (!result) {
+            throw;
+        }
     }
 
     function reveal_bid(uint game_num, uint bid_amount, bytes32 nonce) {
         Game g = games[game_num];
+        next_stage(g);
         if (g.stage != Stages.reveal_bid) {
             throw;
         }
@@ -104,22 +142,89 @@ contract GroupPoker {
         uint refund_amount = g.max_bid_amounts[msg.sender] - bid_amount;
         if (bid_amount > g.bet_size) {
             g.bet_size = bid_amount;
+            g.eligible_to_win[g.bettor] = false;
             g.bettor = msg.sender;
+            g.eligible_to_win[msg.sender] = true;
         }
         if (refund_amount > 0) {
-            result = token_interface.transfer(msg.sender, refund_amount);
+            g.pot_size -= refund_amount;
+            bool result = token_interface.transfer(msg.sender, refund_amount);
             if (!result) {
                 throw;
             }
         }
     }
 
-    function submit_hidden_call(uint game_num, 
+    function submit_hidden_call(uint game_num, bytes32 hidden_call) {
+        Game g = games[game_num];
+        next_stage(g);
+        if (g.stage != Stages.submit_hidden_call) {
+            throw;
+        }
+        if (g.eligible_to_win[msg.sender]) {
+            throw;
+        }
+        g.hidden_calls[msg.sender] = hidden_call;
+        g.pot_size += g.bet_size;
+        bool result = token_interface.transferFrom(msg.sender, address(this), g.bet_size);
+        if (!result) {
+            throw;
+        }
+    }
 
-    // 
-    // all pay auction, the highest amount is the bet that everyone has to match
+    function reveal_call(uint game_num, bool call, bytes32 nonce) {
+        Game g = games[game_num];
+        next_stage(g);
+        if (g.stage != Stages.reveal_call) {
+            throw;
+        }
+        if (sha3(call, nonce) != g.hidden_calls[msg.sender]) {
+            throw;
+        }
+        if (call) {
+            g.eligible_to_win[msg.sender] = true;
+        } else {
+            g.pot_size -= g.bet_size;
+            bool result = token_interface.transfer(msg.sender, g.bet_size);
+            if (!result) {
+                throw;
+            }
+        }
+    }
+    
+    function reveal_hand(uint game_num, uint hand, bytes32 nonce) returns (bool success)  {
+        Game g = games[game_num];
+        next_stage(g);
+        if (g.stage != Stages.reveal_hand) {
+            throw;
+        }
+        if (sha3(hand, nonce) != g.hidden_hands[msg.sender]) {
+            throw;
+        }
+        if (!g.eligible_to_win[msg.sender]) {
+            throw;
+        }
+        uint hand_value = calculate_hand(hand, g.hand_shift);
+        if (hand_value <= g.best_hand) {
+            return false;
+        }
+        g.best_hand = hand_value;
+        g.winner = msg.sender;
+        return true;
+    }
 
-    // multiple games
-
-    //public is generated from block hash + maybe tx id, maybe submitter address
+    function collect_winnings(uint game_num) {
+        Game g = games[game_num];
+        if (g.stage == Stages.reveal_hand) {
+            next_stage(g);
+        }
+        if (g.stage != Stages.collect_winnings) {
+            throw;
+        }
+        if (msg.sender != g.winner) {
+            throw;
+        }
+        g.stage = Stages.finished;
+        token_interface.transfer(msg.sender, g.pot_size);
+    }
 }
